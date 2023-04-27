d9ef909f70ea3d92fb4d59dd68c620aafb157e2d
MiloszPlesniak <105582642+MiloszPlesniak@users.noreply.github.com>
2023-04-27 12:44:50 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 export const App = () => {
-  
+  // s
   return (
     <div
       style={{
@@ -8,7 +8,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template

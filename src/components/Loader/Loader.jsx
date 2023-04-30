84cb14ce7fdee6787661abdb438482f8a624b95b
AngelikaGralewska <angelika.gralewska@gmail.com>
2023-04-30 20:35:35 +0200

    Loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Vortex } from 'react-loader-spinner';
+
+const Loader = isLoading => {
+    return (
+        <Vortex
+        height="80"
+        width="80"
+        ariaLabel="vortex-loading"
+        wrapperStyle={{
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            zIndex: 2,
+        }}
+        wrapperClass="vortex-wrapper"
+        colors={['#E7EAF2', '#C5BAFF', '#FFD8D0','#E7EAF2', '#C5BAFF', '#FFD8D0']}
+        visible={Boolean(isLoading)}
+        />
+    );
+};
+
+Loader.propTypes = {
+    isLoading: PropTypes.bool.isRequired,
+  };
+
+  export default Loader;

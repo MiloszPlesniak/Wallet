6321023cdab4649a0f472a99bbbbfcf79519ea40
MiloszPlesniak <105582642+MiloszPlesniak@users.noreply.github.com>
2023-05-02 10:40:48 +0200

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
-import Navigation from './Navigation/Navigation';
-import RegistrationPage from './RegistrationPage/RegistrationPage';
-import LoginPage from './LoginPage/LoginPage';
+// import Navigation from './Navigation/Navigation';
+// import RegistrationPage from './RegistrationPage/RegistrationPage';
+// import LoginPage from './LoginPage/LoginPage';
 
 const Layout = () => {
   return (

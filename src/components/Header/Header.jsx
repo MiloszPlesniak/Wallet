4ce0fc9fb9c7bfd5dbfbe5458c8ac759925ba34b
BoloMasta <103572991+BoloMasta@users.noreply.github.com>
2023-05-06 20:42:06 +0200

    modal Logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,38 +5,54 @@ import styles from './Header.module.scss';
 import Divider from '@mui/material/Divider';
 import ListItemButton from '@mui/material/ListItemButton';
 import LogoutIcon from '@mui/icons-material/Logout';
+import ModalLogout from 'components/ModalLogout/ModalLogout';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsModalLogoutOpen } from 'redux/global/selectors';
+import { changeIsModalLogoutOpen } from 'redux/global/slice';
 
 const Header = () => {
+  const dispatch = useDispatch();
+
   return (
-    <Media
-      queries={{
-        mobile: '(max-width: 767px)',
-        tablet: '(min-width: 768px) and (max-width: 1279px)',
-        desktop: '(min-width: 1280px)',
-      }}
-    >
-      {matches => (
-        <div className={styles.Header}>
-          <Logo />
-          <div className={styles.Header__logOut}>
-            <span className={styles.Header__logOutUserName}>Name</span>
-            <Divider
-              orientation="vertical"
-              flexItem
-              sx={{ color: '#bdbdbd' }}
-            />
-            <ListItemButton className={styles.Header__logOutButton}>
-              <LogoutIcon
-                sx={{
-                  color: '#bdbdbd',
-                }}
+    <>
+      <Media
+        queries={{
+          mobile: '(max-width: 767px)',
+          tablet: '(min-width: 768px) and (max-width: 1279px)',
+          desktop: '(min-width: 1280px)',
+        }}
+      >
+        {matches => (
+          <div className={styles.Header}>
+            <Logo />
+            <div className={styles.Header__logOut}>
+              <span className={styles.Header__logOutUserName}>Name</span>
+              <Divider
+                orientation="vertical"
+                flexItem
+                sx={{ color: '#bdbdbd' }}
               />
-              <span className={styles.Header__logOutButtonText}>Exit</span>
-            </ListItemButton>
+              <ListItemButton
+                className={styles.Header__logOutButton}
+                onClick={() => dispatch(changeIsModalLogoutOpen())}
+              >
+                <LogoutIcon
+                  sx={{
+                    color: '#bdbdbd',
+                  }}
+                />
+                <span className={styles.Header__logOutButtonText}>Exit</span>
+              </ListItemButton>
+            </div>
           </div>
-        </div>
-      )}
-    </Media>
+        )}
+      </Media>
+
+      <ModalLogout
+        isModalOpen={useSelector(selectIsModalLogoutOpen)}
+        handleModalClose={() => dispatch(changeIsModalLogoutOpen())}
+      />
+    </>
   );
 };
 

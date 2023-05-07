5c93c35cdf6c6bf220030b809abd0a21ebae4c2c
BoloMasta <103572991+BoloMasta@users.noreply.github.com>
2023-05-07 23:15:59 +0200

    stworzenie CurrencySchema i walidacja pobranych z NBP danych

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -23,52 +23,54 @@ const Currency = () => {
       dispatch(fetchCurrency());
     } else {
       const timeDiff = Date.now() - lastFetchDate;
-      if (timeDiff > 3600000) {
+      if (timeDiff > 1000 * 60 * 60) {
         dispatch(fetchCurrency());
       }
     }
   }, [currency, dispatch, lastFetchDate]);
 
   return (
-    <div className={styles.Currency__container}>
-      <div className={styles.Currency__bgGraph} />
-      {isLoading && (
-        <div className={styles.Currency__spinnerContainer}>
-          <CircularProgress className={styles.Currency__spinner} />
-        </div>
-      )}
+    <>
+      {isError && <p>{isError[0]}</p>}
+      <div className={styles.Currency__container}>
+        <div className={styles.Currency__bgGraph} />
+        {isLoading && (
+          <div className={styles.Currency__spinnerContainer}>
+            <CircularProgress className={styles.Currency__spinner} />
+          </div>
+        )}
 
-      {isError && <p>{isError.message}</p>}
-      {!isLoading && !isError && (
-        <table className={styles.Currency__table}>
-          <thead>
-            <tr>
-              <th>Currency</th>
-              <th data-type="bid">Purchase</th>
-              <th data-type="ask">Sale</th>
-              <th></th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {currency?.map(currency => (
-              <tr
-                key={currency.code}
-                onClick={event =>
-                  event.currentTarget.classList.toggle(
-                    styles.Currency__row_selected
-                  )
-                }
-              >
-                <td>{currency.code}</td>
-                <td data-type="bid">{currency.bid.toFixed(4)}</td>
-                <td data-type="ask">{currency.ask.toFixed(4)}</td>
+        {!isLoading && !isError && (
+          <table className={styles.Currency__table}>
+            <thead>
+              <tr>
+                <th>Currency</th>
+                <th data-type="bid">Purchase</th>
+                <th data-type="ask">Sale</th>
+                <th></th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
+            </thead>
+
+            <tbody>
+              {currency?.map(currency => (
+                <tr
+                  key={currency.code}
+                  onClick={event =>
+                    event.currentTarget.classList.toggle(
+                      styles.Currency__row_selected
+                    )
+                  }
+                >
+                  <td>{currency.code}</td>
+                  <td data-type="bid">{currency.bid.toFixed(4)}</td>
+                  <td data-type="ask">{currency.ask.toFixed(4)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </>
   );
 };
 
